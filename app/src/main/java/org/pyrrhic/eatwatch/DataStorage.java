d83aa30997346ea5b670706fd5b2b5f9d81cdd9c
bittercode <bittercode@gmail.com>
2014-01-31 22:06:16 +0100

    Created empty class for managing local storage.

---
+++ app/src/main/java/org/pyrrhic/eatwatch/DataStorage.java
@@ -0,0 +1,13 @@
+package org.pyrrhic.eatwatch;
+
+/**
+ * Created by jr.peck on 1/31/14.
+ * This file will be used to manage local storage for my EatWatch app
+ */
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+
+public class DataStorage {
+}
